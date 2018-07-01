785ece2d6577706cee2a6c3c3f75b55563b52fed
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2018-07-01 21:55:39 +0700

    mode full test dengan highcharts

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const url = require('url');
 
 app.on('ready', () => {
     var main = new BrowserWindow();
-    // main.loadURL(url.format({
-    //     pathname: path.join(__dirname, 'src/index.html'),
-    //     protocol: 'file:',
-    //     slashes: true
-    // }));
-    main.loadURL('http://127.0.0.1:5500/src/plot.html')
+    main.loadURL(url.format({
+        pathname: path.join(__dirname, 'src/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    // main.loadURL('http://127.0.0.1:5500/src/plot.html')
     main.webContents.openDevTools();
 });
