77706571c80999229858cf8f246b393266f69adc
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2018-07-01 21:45:45 +0700

    tambah graph highcharts

--- src/components/grafik-container/grafik-container.js
+++ src/components/grafik-container/grafik-container.js
@@ -6,8 +6,9 @@ angular.module('myApp')
             removeMarker: '&'
         },
         controller: class grafikContainer {
-            constructor($scope) {
+            constructor($scope, $timeout) {
                 this.scope = $scope;
+                this.timeout = $timeout;
 
                 this.chromeTabs = new ChromeTabs();
                 const el = document.querySelector('.chrome-tabs');
@@ -68,11 +69,85 @@ angular.module('myApp')
                 };
                 this.scope.graphs.push(graph);
 
+                // inisialisasi graph setelah scope di render semua
+                this.timeout(() => {
+                    console.log('siap render graph', document.getElementById(graph.divId));
+                    initGraph(graph.divId);
+                });
+
+
                 // add tab terakhir agar tidak error di event el di container
                 this.chromeTabs.addTab({
                     title: tab.title,
                     id: tab.id
                 });
+
+                // fungsi template untuk get graph
+                function initGraph(divId) {
+                    Highcharts.chart(divId, {
+
+                        title: {
+                            text: `ini id ke-${divIdF}`
+                        },
+
+                        subtitle: {
+                            text: 'Source: thesolarfoundation.com'
+                        },
+
+                        yAxis: {
+                            title: {
+                                text: 'Number of Employees'
+                            }
+                        },
+                        legend: {
+                            layout: 'vertical',
+                            align: 'right',
+                            verticalAlign: 'middle'
+                        },
+
+                        plotOptions: {
+                            series: {
+                                label: {
+                                    connectorAllowed: false
+                                },
+                                pointStart: 2010
+                            }
+                        },
+
+                        series: [{
+                            name: 'Installation',
+                            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+                        }, {
+                            name: 'Manufacturing',
+                            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+                        }, {
+                            name: 'Sales & Distribution',
+                            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+                        }, {
+                            name: 'Project Development',
+                            data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+                        }, {
+                            name: 'Other',
+                            data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+                        }],
+
+                        responsive: {
+                            rules: [{
+                                condition: {
+                                    maxWidth: 500
+                                },
+                                chartOptions: {
+                                    legend: {
+                                        layout: 'horizontal',
+                                        align: 'center',
+                                        verticalAlign: 'bottom'
+                                    }
+                                }
+                            }]
+                        }
+
+                    });
+                }
             }
 
             $onChanges({ markerIdCount }) {
@@ -125,6 +200,9 @@ angular.module('myApp')
             </div>
             <div style="background-color: rgb(242, 242, 242); position: absolute; height: calc(100% - 55px); width: 100%;">
                 <div ng-repeat="graph in graphs" ng-attr-id="{{graph.divId}}" ng-show="graph.show" style="width: 100%; height: 100%;">hallo id-{{graph.id}}</div>
+                <!--      
+                    <div id="tesGraph">hallo</div>
+                -->
             </div>
         </div>
         `
